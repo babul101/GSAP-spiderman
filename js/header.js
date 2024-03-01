c438952322f50b4608d466bd5b927fcb814798f8
babul101 <babuldeb101@gmail.com>
2024-03-01 14:23:22 +0530

    finished the carousel header

--- js/header.js
+++ js/header.js
@@ -1,16 +1,21 @@
-const carousels = document.querySelectorAll('header h1,header h2')
-const fadeInTimeline = gsap.timeline()
+const carousels = document.querySelectorAll("header h1,header h2");
+const fadeInTimeline = gsap.timeline();
 
 fadeInTimeline
-    .set(carousels,{opacity:0})
-    .to(carousels,{opacity:1,delay:1,stagger:1,duration:3})
+  .set(carousels, { opacity: 0 })
+  .to(carousels, { opacity: 1, delay: 1, stagger: 1, duration: 3 });
 
+carousels.forEach((carousel) => {
 
-const movementTimeline = gsap.timeline({repeat:-1})
+    const spanTag = carousel.querySelector('span')
+    const spanWidth = spanTag.clientWidth
 
-movementTimeline
-    .set(carousels,{x:0})
-    .to(carousels,{x:-400,duration:6,ease:"linear"})
+    for(let i=0;i<20 ;i++){
+        carousel.appendChild(spanTag.cloneNode(true))
+    }
+  const movementTimeline = gsap.timeline({ repeat: -1 });
 
-
-
+  movementTimeline
+    .set(carousel, { x: 0 })
+    .to(carousel , { x: spanWidth * -1, duration: 6, ease: "linear" });
+});
